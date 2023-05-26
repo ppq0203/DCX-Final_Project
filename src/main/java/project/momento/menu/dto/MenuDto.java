197afc9f88964961228d6f2140cfe6c698d9aed3
ASUS <ASUS@LAPTOP-JFIHP3H8>
2023-05-26 09:09:46 +0900

    Auth, Menu, Question, User Dto 추가

---
+++ src/main/java/project/momento/menu/dto/MenuDto.java
@@ -0,0 +1,38 @@
+package project.momento.menu.dto;
+
+import org.springframework.context.annotation.PropertySource;
+
+import lombok.Data;
+
+@Data
+@PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
+public class MenuDto {
+	
+	/*메뉴 고유식별 번호*/
+	private int pkMenuSeq;
+	/*상위 메뉴번호*/
+	private int upperMenuNo;
+	/*메뉴 등급*/
+	private int level;
+	/*메뉴명*/
+	private String menuName;
+	/*링크 URL*/
+	private String linkUrl;
+	/*Controller path*/
+	private String path;
+	/*사용여부*/
+	private String useYn;
+	/*삭제여부*/
+	private String delYn;
+	/*등록자 아이디*/
+	private String registId;
+	/*등록일자*/
+	private String registDt;
+	/*수정자 아이디*/
+	private String updateId;
+	/*수정일자*/
+	private String updateDt;
+	/*권한 고유 식별번호*/
+	private int pkAuthSeq;
+
+}
