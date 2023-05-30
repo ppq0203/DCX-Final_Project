07bb5454c9b33ac15348602b689d8bc5487ddc7b
ASUS <ASUS@LAPTOP-JFIHP3H8>
2023-05-30 13:02:35 +0900

    이승주 2023-05-30 my page 메뉴 불러오기 및 메뉴 수정, 관리 기능 변경

--- src/main/java/project/momento/mypage/controller/MypageController.java
+++ src/main/java/project/momento/mypage/controller/MypageController.java
@@ -1,25 +1,18 @@
 package project.momento.mypage.controller;
 
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import project.momento.chart.dto.ChartDto;
-import project.momento.chart.service.ChartService;
 import project.momento.login.dto.LoginDto;
+import project.momento.login.service.LoginService;
 import project.momento.menu.dto.MenuDto;
 import project.momento.menu.service.MenuService;
 import project.momento.page.Criteria;
-import project.momento.page.Paging;
 
 @Controller
 public class MypageController {
@@ -29,42 +22,28 @@ public class MypageController {
 	 */
 	
 	@Autowired
-	private ChartService chartService;
 	private MenuService menuService;
+	@Autowired
+	private LoginService loginService;
 
 	/*
 	 * 사이트에서 이동
 	 */
 	@RequestMapping(value="/mypage.com", produces="application/text;charset=utf-8") /* value주소 이름*/
-	public ModelAndView Mypage(Criteria cri, Model model, HttpServletRequest request) {
-		
-		ModelAndView mv = new ModelAndView("content/mypage");
-		LoginDto loginDto = (LoginDto) request.getSession().getAttribute("loginDto");
-		cri.setPkUserSeq(loginDto.getPkUserSeq());
-		int auth = loginDto.getPkAuthSeq();
-		if(auth != 3)
-		{
-			mv.addObject("role", "Admin");
+	public String Mypage(Criteria cri, Model model, HttpServletRequest request) {
+		// 세션에서 내 정보를 가져온다
+		LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
+		// 로그인이 되어있는지 확인
+		if(!(loginDto == null)) {
+			// 로그인이 되어있는 경우
+			List<MenuDto> menuList = menuService.getMenuList(loginDto.getPkAuthSeq());
+			System.out.println(menuList);
+			model.addAttribute("menuList", menuList);
+			return "content/mypage";
+		} else {
+			// 로그인이 되어있지 않은 경우
+			return "/login.com";
 		}
-		else
-		{
-			mv.addObject("role", "Student");
-		}
-		int total = 1;
-//		total = chartService.getProductDetailListCount(cri);
-//		List<ChartDto> list = chartService.getResultList(cri);
-		// 페이징 객체
-        Paging paging = new Paging();
-        paging.setCri(cri);
-        paging.setTotalCount(total); 
-        System.out.println(paging);
-        System.out.println(cri);
-//		List<ChartDto> detailList = chartService.getProductDetailList(cri);
-		
-//		model.addAttribute("list", list);
-//		mv.addObject("detailList", detailList);
-		mv.addObject("paging", paging);   
-		return mv;
 	}
 	
 	@RequestMapping(value="/menumanagement.com", produces="application/text;charset=utf-8")
@@ -87,19 +66,25 @@ public class MypageController {
 		return "/main.com";
 	}
 	
-	@RequestMapping(value="/getmenu.com", produces="application/text;charset=utf-8") /* value주소 이름*/
-	public String GetMenu(Model model, HttpServletRequest request) {
-		// 세션에서 내 정보를 가져온다
-		LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
-		// 로그인이 되어있는지 확인
-		if(!(loginDto == null)) {
-			// 로그인이 되어있는 경우
-			List<MenuDto> menuList = menuService.getMenuList(loginDto.getPkAuthSeq());
-			model.addAttribute("menuList", menuList);
-			return "/mypage.com";
-		}
-		// 로그인이 되어있지 않은 경우
-		return "/main.com";
-	}
+//	@RequestMapping(value="/getmenu.com", produces="application/json;charset=utf-8") /* value주소 이름*/
+//	public String GetMenu(Model model, HttpServletRequest request) {
+//		// 세션에서 내 정보를 가져온다
+//		// LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
+//		LoginDto testDto = new LoginDto();
+//		testDto.setUserId("test");
+//		testDto.setPassword("asdf");
+//		LoginDto loginDto = loginService.checkLogin(testDto);
+//		// 로그인이 되어있는지 확인
+//		if(!(loginDto == null)) {
+//			// 로그인이 되어있는 경우
+//			List<MenuDto> menuList = menuService.getMenuList(loginDto.getPkAuthSeq());
+//			System.out.println(menuList);
+//			model.addAttribute("menuList", menuList);
+//			return "redirect:/mypage.com";
+//		} else {
+//			// 로그인이 되어있지 않은 경우
+//			return "/login.com";
+//		}
+//	}
 	
 }
