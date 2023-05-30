d891972450aab3c1286e29ed9b344f7147a10e74
ppq0203 <ppq23@naver.com>
2023-05-30 09:22:01 +0900

    temp

---
+++ 11/22/Solution.java
@@ -0,0 +1,11 @@
+public class Solution{
+
+    public Solution() {
+        System.out.println("Load MyClass");
+    }
+
+    @Override
+  public String toString() {
+    return "This is MyClass";
+  }
+}
