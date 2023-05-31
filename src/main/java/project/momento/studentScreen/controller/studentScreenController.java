9d0acd879b328cb9cd0c110a85b0c2ad508268f7
SMchoi <fpahsgkdla5421@gmail.com>
2023-05-31 18:21:18 +0900

    과거여행 2회차.

--- src/main/java/project/momento/studentScreen/controller/studentScreenController.java
+++ src/main/java/project/momento/studentScreen/controller/studentScreenController.java
@@ -13,23 +13,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.servlet.http.HttpServletRequest;
+import project.momento.login.dto.LoginDto;
 import project.momento.sign.dto.SignDto;
 import project.momento.sign.service.SignService;
 
 @Controller
-public class StudentScreenController {
+public class studentScreenController {
 	
 	@Autowired
 	private SignService SignService;
 	
 	/*
-	 * 학생 화면 페이지 이동
+	 * 학생 회원 관리 이동
 	 * param
-	 * return contents/studentScreen 받는값
+	 * return contents/sign 받는값
 	 */
-	@RequestMapping(value="/studentScreen.com", produces="application/text;charset=utf-8") /* value주소 불러오기 이름*/
-	public String studentScreenMain( Model model) {
-	    return "content/studentScreen";
+	@RequestMapping(value="/signManage.com", produces="application/text;charset=utf-8") /* value주소 불러오기 이름*/
+	public String SignManage(Model model, SignDto signDto, HttpServletRequest request) {
+		List<String> wList = SignService.waitList(signDto);
+		//가입대기중인 계정을 모아 모델로 넘겨주기.
+		model.addAttribute("wait", wList);
+		
+		System.out.println(model.getAttribute("wait"));
+	    return "content/signManage";
+	}
+	
+	@RequestMapping(value="/signUser.com", produces="application/text;charset=utf-8")
+	public void permitsign(Model model, SignDto signDto, HttpServletRequest request) {
+		//전체수락이 체크되지 않은 경우.
+//		if(request.getParameter("check"))
+//		{
+//			for(SignDto permit : request.getParameterValues(checklist))
+//			{
+//				SignService.insertUser(permit);
+//			}
+//		}
+//		//전체수락이 체크된 경우.
+//		else
+//		{
+//			SignService.signUser(signDto);
+//		}
 	}
 	
 }
