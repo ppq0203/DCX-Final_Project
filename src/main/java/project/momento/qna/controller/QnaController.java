dc8770fc68ab9d5a17fbe09e8b4faa20d068565f
anyware <anyware@LAPTOP-LRVQFOST>
2023-05-31 18:15:33 +0900

    2023.05.31.김정헌.학생화면(강의일정,보충영상,qna 수정 페이지)

--- src/main/java/project/momento/qna/controller/QnaController.java
+++ src/main/java/project/momento/qna/controller/QnaController.java
@@ -1,90 +1,46 @@
-package project.momento.mypage.controller;
+package project.momento.qna.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
+import project.momento.chart.dto.ChartDto;
+import project.momento.chart.service.ChartService;
 import project.momento.login.dto.LoginDto;
-import project.momento.login.service.LoginService;
-import project.momento.menu.dto.MenuDto;
-import project.momento.menu.service.MenuService;
 import project.momento.page.Criteria;
+import project.momento.page.Paging;
 
 @Controller
-public class MypageController {
+public class QnaController {
 	
 	/*
 	 * @Autowired private SignService SignService;
 	 */
 	
-	@Autowired
-	private MenuService menuService;
-	@Autowired
-	private LoginService loginService;
-
+	 @Autowired 
+	 private ChartService chartService;
+	 
 	/*
-	 * 사이트에서 이동
+	 * calendar사이트로 이동
 	 */
-	@RequestMapping(value="/mypage.com", produces="application/text;charset=utf-8") /* value주소 이름*/
-	public String Mypage(Criteria cri, Model model, HttpServletRequest request) {
-		// 세션에서 내 정보를 가져온다
-		LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
-		// 로그인이 되어있는지 확인
-		if(!(loginDto == null)) {
-			// 로그인이 되어있는 경우
-			List<MenuDto> menuList = menuService.getMenuList(loginDto.getPkAuthSeq());
-			System.out.println(menuList);
-			model.addAttribute("menuList", menuList);
-			return "content/mypage";
-		} else {
-			// 로그인이 되어있지 않은 경우
-			return "/login.com";
-		}
+	@RequestMapping(value="/qna.com", produces="application/text;charset=utf-8") /* value주소 이름*/
+	public ModelAndView calendarMain(Criteria cri, Model model, ChartDto chartDto) {
+		ModelAndView mv = new ModelAndView("content/qna"); 
+		return mv;
 	}
-	
-	@RequestMapping(value="/menumanagement.com", produces="application/text;charset=utf-8")
-	public String MenuManagement(Model model, HttpServletRequest request) {
-		// 세션에서 내 정보를 가져온다
-		LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
-		// 로그인이 되어있는지 확인
-		if(!(loginDto == null)) {
-			// 로그인이 되어 있을 경우
-			// 관리자 권한이 맞는지 확인
-			if(loginDto.getPkAuthSeq() == 1) {
-				// 관리자 권한인 경우
-				menuService.menuManagement((MenuDto)model.getAttribute("menuManageDto"));
-				return "/mypage.com";
-			}
-			// 관리자 권한이 아닌 경우
-			return "/mypage.com";
-		}
-		// 로그인이 되어있지 않은 경우
-		return "/main.com";
-	}
-	
-//	@RequestMapping(value="/getmenu.com", produces="application/json;charset=utf-8") /* value주소 이름*/
-//	public String GetMenu(Model model, HttpServletRequest request) {
-//		// 세션에서 내 정보를 가져온다
-//		// LoginDto loginDto = (LoginDto)request.getSession().getAttribute("loginDto");
-//		LoginDto testDto = new LoginDto();
-//		testDto.setUserId("test");
-//		testDto.setPassword("asdf");
-//		LoginDto loginDto = loginService.checkLogin(testDto);
-//		// 로그인이 되어있는지 확인
-//		if(!(loginDto == null)) {
-//			// 로그인이 되어있는 경우
-//			List<MenuDto> menuList = menuService.getMenuList(loginDto.getPkAuthSeq());
-//			System.out.println(menuList);
-//			model.addAttribute("menuList", menuList);
-//			return "redirect:/mypage.com";
-//		} else {
-//			// 로그인이 되어있지 않은 경우
-//			return "/login.com";
-//		}
-//	}
 	
 }
