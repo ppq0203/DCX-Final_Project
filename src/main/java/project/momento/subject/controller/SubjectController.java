0d3febadad3267f6233fec6e0a9355d63bd57000
dkqydhd <126450338+dkqydhd@users.noreply.github.com>
2023-06-01 18:47:04 +0900

    2023-06-01 최진명 일정 달력 표시 기능 구현(진행중)

--- src/main/java/project/momento/subject/controller/SubjectController.java
+++ src/main/java/project/momento/subject/controller/SubjectController.java
@@ -21,18 +21,18 @@ public class SubjectController {
 	
 	@RequestMapping(value="/subject.com", produces="application/text;charset=utf-8") /* value주소 이름*/
 	public String selectSubject(Model model, SubjectDto subjectDto,HttpServletRequest request){
-	int pkSubjectSeq = subjectDto.getPkSubjectSeq();
+	int pkSubjectSeq = 1;//subjectDto.getPkSubjectSeq();
 	//Dto에서 pkSubjectSeq를 가져와 subjectDto에 담는다.
 	subjectDto.setPkSubjectSeq(pkSubjectSeq);
 	//만약 pkSubjectSeq값이 0보다 크면 해당로직을 실행한다.
 	if(pkSubjectSeq>0) {
 		SubjectDto dto = subjectService.selectSubject(subjectDto);
-		List<SubjectDto> list = new ArrayList<>();
-	//DB에서 꺼내온 값을 변수 dto에 넣고 그 값을 변수 list에 담는다.	
-		list.add(dto);
-	//session을 호출하여 list라는 속성에 list값을 담아놓는다.	
-		request.getSession().setAttribute("list", list);
-						}
+//		List<SubjectDto> list = new ArrayList<>();
+//	//DB에서 꺼내온 값을 변수 dto에 넣고 그 값을 변수 list에 담는다.	
+//		list.add(dto);
+//	//session을 호출하여 list라는 속성에 list값을 담아놓는다.	
+//		request.getSession().setAttribute("list", list);
+		model.addAttribute(dto)		;		}
 		return "content/calendar";
 																								}
 								}
