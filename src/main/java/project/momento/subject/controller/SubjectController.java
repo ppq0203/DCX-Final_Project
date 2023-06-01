7fb950cdc45f447f0160f8a0a2369afa04dfd65d
dkqydhd <126450338+dkqydhd@users.noreply.github.com>
2023-06-01 09:12:57 +0900

    dump

--- src/main/java/project/momento/subject/controller/SubjectController.java
+++ src/main/java/project/momento/subject/controller/SubjectController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import project.momento.subject.dto.SubjectDto;
 import project.momento.subject.service.SubjectService;
 
@@ -19,25 +20,19 @@ public class SubjectController {
 	
 	
 	@RequestMapping(value="/subject.com", produces="application/text;charset=utf-8") /* value주소 이름*/
-	public String selectSubject(Model model, SubjectDto subjectDto){
-//		int pkSubjectSeq = subjectDto.getPkSubjectSeq();
-		//Dto에서 pkSubjectSeq를 가져와 subjectDto에 담는다.
-		subjectDto.setPkSubjectSeq(1);
-//		if(1>0) {
-			subjectService.selectSubject(subjectDto);
-			System.out.println(subjectDto);
-//			SubjectDto dto = subjectService.selectSubject(subjectDto);
-//			List<SubjectDto> list = new ArrayList<>();
-//			for (int i = 0; i < list.size(); i++) {
-//				list.add(dto);
-//				model.addAttribute("subjectList", list);
-//													}
-//			System.out.println(model);
-//							}
-		
-		
-
-		
+	public String selectSubject(Model model, SubjectDto subjectDto,HttpServletRequest request){
+	int pkSubjectSeq = subjectDto.getPkSubjectSeq();
+	//Dto에서 pkSubjectSeq를 가져와 subjectDto에 담는다.
+	subjectDto.setPkSubjectSeq(pkSubjectSeq);
+	//만약 pkSubjectSeq값이 0보다 크면 해당로직을 실행한다.
+	if(pkSubjectSeq>0) {
+		SubjectDto dto = subjectService.selectSubject(subjectDto);
+		List<SubjectDto> list = new ArrayList<>();
+	//DB에서 꺼내온 값을 변수 dto에 넣고 그 값을 변수 list에 담는다.	
+		list.add(dto);
+	//session을 호출하여 list라는 속성에 list값을 담아놓는다.	
+		request.getSession().setAttribute("list", list);
+						}
 		return "content/calendar";
-	}
-}
+																								}
+								}
