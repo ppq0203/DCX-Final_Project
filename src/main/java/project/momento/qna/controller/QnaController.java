f314cfada4c8168cb97c4429775f9196db6105b7
anyware <anyware@LAPTOP-DB6L0KN9>
2023-06-03 10:04:00 +0900

    20230602 정윤재 화면분할

--- src/main/java/project/momento/qna/controller/QnaController.java
+++ src/main/java/project/momento/qna/controller/QnaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -27,8 +28,15 @@ public class QnaController {
 	private LoginService loginService;
 
 	/*
-	 * 사이트에서 이동
-	 *
+	 * Q&A 페이지 이동
+	 * param
+	 * return contents/qnaforms 받는값
 	 */
+	@RequestMapping(value="/{userDivn}/qna/main", produces="application/text;charset=utf-8") /* value주소 불러오기 이름*/
+	public String qnasMain(@PathVariable String userDivn, Model model) {
+	    
+		
+		return "content/"+userDivn+"/qna/qna";
+	}
 
 }
