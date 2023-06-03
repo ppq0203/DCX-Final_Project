0e2715c48be8b21e01f1f833dd6ad90cb5fdf827
toyoy <toyoy@DESKTOP-SOEVQEH>
2023-06-03 23:37:03 +0900

    20230603 정윤재 subject -> education으로 변경

--- src/main/java/project/momento/education/dto/EducationDto.java
+++ src/main/java/project/momento/education/dto/EducationDto.java
@@ -1,32 +1,33 @@
-package project.momento.subject.dto;
+package project.momento.education.dto;
 
 import org.springframework.context.annotation.PropertySource;
 
 import lombok.Data;
+import project.momento.login.dto.LoginDto;
 
 @Data
 @PropertySource(value = "classpath:application.properties", encoding = "UTF-8")
 /*강의관리*/
-public class SubjectDto {
+public class EducationDto extends LoginDto {
 	
 	/*과목 고유 식별 번호*/
-	private int pkSubjectSeq;
+	private int pkEducationSeq;
 	/*교육 이름*/  
-	private String subjectName;
+	private String educationName;
 	/*교육 상태(A: 준비, B: 진행, C: 종료)*/  
-	private String subjectState;
+	private String educationState;
 	/*강의 시작일자*/  
-	private String subjectDt;
+	private String educationDt;
 	/*강의 시작일자*/  
 	private String subStartDt;
 	/*강의 종료일자*/  
 	private String subEndDt;
 	/*담당 선생*/  
-	private String subjectTeather;
+	private String educationTeather;
 	/*과목의 시험*/  
-	private String subjectTest;
+	private String educationTest;
 	/*시험 날짜*/  
-	private String subjectTestDt;
+	private String educationTestDt;
 	/*등록 아이디*/  
 	private String registId;
 	/*등록 일자*/  
@@ -39,12 +40,8 @@ public class SubjectDto {
 	private int pkManagerSeq;
 	/*학생 고유 식별 번호*/
 	private int pkUserSeq;
-	/*관리자 고유 식별 번호*/
-	private int[] pkManagerSeqArray;
-	/*학생 고유 식별 번호*/
-	private int[] pkUserSeqArray;
 	/*과목 명*/  
-	private String lectureName;
+	private String subjectName;
 	
 	
 	
